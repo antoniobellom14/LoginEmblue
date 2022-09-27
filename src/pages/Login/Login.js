0aeb1cd82ef4af901779fe16e74c31ed72e90b23
Antonio Bello <antonio.b@embluemail.com>
2022-09-27 16:16:27 -0300

    avance login

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Title from './components/Title/Title';
+
+const Login = () =>{
+    return(
+        <div>
+            <Title text ='soy un titulo'/>
+        
+        </div>
+    )
+};
+
+export default Login
---
+++ src/pages/Login/components/Title/Title.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Title = ({ text }) => {
+
+    return (
+        <div className='title'>
+            <label>
+                {text}
+            </label>
+        </div>
+    )
+};
+
+export default Title
